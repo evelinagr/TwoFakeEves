5df331a832b7a9e37b639ebc99da32e30646d686
37129 <evelinagrebneva@inbox.lv>
2022-02-23 20:41:30 +0200

    Failure push

---
+++ 2nd task/failure.js
@@ -0,0 +1,21 @@
+
+let answer1 = this.prompt("Please enter the 1st number");
+console.log(answer1);
+
+let operator = this.prompt("Please enter the operator");
+console.log(operator);
+
+let answer2 = this.prompt("Please enter the 2nd number");
+console.log(answer2);
+
+
+if(operator == "+")
+    this.alert("answer1" + "answer2");
+else if (operator == "-")
+    this.alert("answer1" - "answer2");
+else if (operator == "*")
+    this.alert("answer1" * "answer2");
+else if (operator == "/")
+    this.alert("answer1" / "answer2");
+else
+    this.alert("error");
