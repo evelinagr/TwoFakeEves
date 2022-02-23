0d3ffd9cd96924c188419c2a46d6a1dbec458dc6
37129 <evelinagrebneva@inbox.lv>
2022-02-23 12:19:24 +0200

    2nd task commit

---
+++ 2nd task/newscript.js
@@ -0,0 +1,16 @@
+function showAlert(){
+var answer1 = parseFloat(document.getElementById("answer1").value);
+var answer2 = parseFloat (document.getElementById("answer2").value);
+var operator = document.getElementById("operator").value;
+
+if(operator == "+")
+    this.alert(document.getElementById.value = answer1+answer2);
+else if (operator == "-")
+    this.alert(document.getElementById.value = answer1-answer2);
+else if (operator == "*")
+    this.alert(document.getElementById.value = answer1*answer2);
+else if (operator == "/" || ":")
+    this.alert(document.getElementById.value = answer1/answer2);
+else
+    this.alert("Error");
+}
